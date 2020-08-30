ca118769b39b4b49e4d05a9ebac0f930ca286f7f
Jihoon-Ryu <ujihun94@naver.com>
2020-08-30 17:07:03 +0200

    .
    .

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request,redirect
-from scarpper import get_jobs
+from flask import Flask, render_template, request,redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
 
-
+db ={}
 
 @app.route("/")
 def home():
@@ -13,12 +14,40 @@ def home():
 def report():
   word= request.args.get('word')
   if word:
-   word = word.lower()
-   jobs=get_jobs(word)
-   print(jobs)
+    word = word.lower()
+    existingJobs = db.get(word)
+    if existingJobs:
+     jobs=existingJobs
+     
+    else:  
+     #if exist not in db, execute scrapping
+     jobs=get_jobs(word)
+     #save jobs list in db
+     db[word] = jobs
   else: 
    return redirect("/")
-  return render_template("report.html", searchingBy =word)
+  
+  return render_template("report.html", searchingBy =word,resultNumber=len(jobs), jobs=jobs)
+
+@app.route("/export")
+def export():
+  try: 
+    word= request.args.get('word')
+    if not word:
+     raise Exception  
+    
+    word= word.lower()
+    jobs=db.get(word)
+    if not jobs:
+     raise Exception
+    
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+  except:  
+    return redirect("/")
+  
+    
+
 
 app.run(host="0.0.0.0")
 
